1a7f47ab6514893cc848654cd367af4a617c4be3
seoeungi <sjlim02255@gmail.com>
2024-12-04 23:20:33 +0900

    init kotlin config server

---
+++ src/test/kotlin/com/planverse/configurationserver/ConfigurationServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.planverse.configurationserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
